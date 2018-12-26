9c1c29fa486723d02306d635673352129fd467a7
apiliuk <lohika.artur@98point6.com>
2018-12-26 12:07:55 +0200

    fix

--- conftest.py
+++ conftest.py
@@ -5,3 +5,4 @@ from tests.utils.api import Api
 @pytest.fixture(scope="function", autouse=True)
 def pytest_runtest_setup():
     Api.login()
+    yield
--- tests/create_issue_test.py
+++ tests/create_issue_test.py
@@ -1,12 +1,19 @@
+import pytest
 from requests import Response
 from tests.utils.api import Api
 
 
 class TestIssueRefactored:
 
-    def setup(self):
-        1 == 1
+    @staticmethod
+    def setup():
+        assert 1 == 1
 
+    @staticmethod
+    def teardown():
+        assert 1 == 1
+
+    @pytest.mark.api
     def test_create_issue_refactored(self):
         result: Response = Api.create_issue("WEBINAR")
 
